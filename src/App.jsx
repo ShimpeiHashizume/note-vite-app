31dc745dda52c9d72f611e015bf5bb218fd930ee
hashizume <hashizume@fabric-arts.co.jp>
2023-11-07 10:49:49 +0900

    [update]ノートを編集する機能を実装

--- src/App.jsx
+++ src/App.jsx
@@ -29,10 +29,21 @@ function App() {
     return notes.find((note) => note.id === activeNote);
   };
 
+  const onUpdateNote = (updatedNote) => {
+    const updatedNotesArray = notes.map((note) => {
+      if (note.id === updatedNote.id) {
+        return updatedNote;
+      } else {
+        return note;
+      }
+    });
+    setNotes(updatedNotesArray);
+  };
+
   return (
     <div className="App">
       <Sidebar onAddNote={onAddNote} notes={notes} onDeleteNote={onDeleteNote} activeNote={activeNote} setActiveNote={setActiveNote} />
-      <Main activeNote={getActiveNote()} />
+      <Main activeNote={getActiveNote()} onUpdateNote={onUpdateNote} />
     </div>
   );
 }
